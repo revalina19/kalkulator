d6401a87f5fb073ddcc567fc06a8146089ab80f5
revalina19 <113223272+revalina19@users.noreply.github.com>
2022-09-10 10:13:13 +0700

    Add files via upload

---
+++ kalkulator.py
@@ -0,0 +1,26 @@
+angka1 = 8
+angka2 = 2
+
+# membuat penjumlahan
+jumlah = angka1 + angka2
+
+# membuat pengurangan
+kurang = angka1 - angka2
+
+# membuat perkalian
+kali = angka1 * angka2
+
+# membuat pembagian
+bagi = angka1 / angka2
+
+# hasil 8 ditambah 2 sama dengan = 10
+print(f"hasil {angka1} ditambah {angka2} sama dengan=", jumlah)
+
+# hasil 8 dikurangi 2 sama dengan = 6
+print(f"hasil {angka1} dikurangi {angka2} sama dengan=", kurang)
+
+# hasil 8 dikali 2 sama dengan = 16
+print(f"hasil {angka1} dikali {angka2} sama dengan=", kali)
+
+# hasil 8 dibagi 2 sama dengan = 4
+print(f"hasil {angka1} dibagi {angka2} sama dengan=", bagi)
